8a5ca997c1a046febcd8ff600134ada7345dc5f0
XodeJunkie <tolulopeakindeko@gmail.com>
2020-10-29 22:10:35 +0100

    login widget

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:fad_quizapp/Screens/Welcome/components/background.dart';
+import 'package:fad_quizapp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -8,7 +10,32 @@ class Body extends StatelessWidget {
     // ratio to screen
     return Background(
       child: Column(
-        children: <Widget>[],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Welcome to FAD QUIZ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SvgPicture.asset(
+            "assets/icons/chat.svg",
+            height: size.height * 0.45,
+          ),
+          Container(
+            width: size.width * 0.6,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                color: kPrimaryColor,
+                onPressed: () {},
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
